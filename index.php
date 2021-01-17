3b0d3644dd13f14ae17a55d17858655205529982
RODRIGO MONTEBELLO SABOYA BRITO <blackknight@blackknightstudio.com.br>
2021-01-17 19:06:15 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<h1> Proof of Knowledge</h1>
+<script type="text/javascript">
+  alert ("Hello world");
+  for (var i = 0; i < 50; i++){
+    if(i === 0)document.write("<h3>");
+    document.write(" " + i);
+    if(i === 49)document.write("</h3>");
+  }
+  for (var i = 0; i < 50; i++){
+    document.write("<h3>" + i + "</h3>");
+  }
+</script>
+</body>
+</html>
